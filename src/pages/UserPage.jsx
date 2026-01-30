5bb4b97a5a4f1cb24174df6cbdf882231c941ef4
ZaaeAlfrz21 <reza.alfrz21@gmail.com>
2026-01-30 18:49:25 +0700

    Update UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -7,7 +7,7 @@ import {
 } from 'react-icons/fa';
 import axios from 'axios'; 
 
-const API_URL = 'celebrated-analysis-production-7785.up.railway.app';
+const API_URL = 'https://celebrated-analysis-production-7785.up.railway.app';
 
 const UserPage = () => {
     // Ambil user, logout, dan login (untuk update state) dari Context
